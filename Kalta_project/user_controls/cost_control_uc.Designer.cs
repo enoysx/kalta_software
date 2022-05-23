8a89c1adcd416fb34d32e19adf7418fa945f3a2e
enoysx <88864833+enoysx@users.noreply.github.com>
2022-05-23 09:24:39 +0700

    change a little bit

--- Kalta_project/user_controls/cost_control_uc.Designer.cs
+++ Kalta_project/user_controls/cost_control_uc.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dgv_report_grpCtrl = new System.Windows.Forms.TabControl();
             this.breakdown_rap = new System.Windows.Forms.TabPage();
+            this.advancedDataGridView1 = new ADGV.AdvancedDataGridView();
             this.breakdown_po = new System.Windows.Forms.TabPage();
             this.dgv_breakdown_po_grpCtrl = new System.Windows.Forms.DataGridView();
             this.bpk_kantor = new System.Windows.Forms.TabPage();
@@ -47,10 +48,10 @@
             this.input_data = new System.Windows.Forms.GroupBox();
             this.export_report = new System.Windows.Forms.GroupBox();
             this.btn_report = new System.Windows.Forms.Button();
-            this.grp_box_search = new System.Windows.Forms.GroupBox();
-            this.advancedDataGridView1 = new ADGV.AdvancedDataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dgv_report_grpCtrl.SuspendLayout();
             this.breakdown_rap.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.advancedDataGridView1)).BeginInit();
             this.breakdown_po.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_breakdown_po_grpCtrl)).BeginInit();
             this.bpk_kantor.SuspendLayout();
@@ -61,7 +62,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgv_po_kantor_grpCtrl)).BeginInit();
             this.input_data.SuspendLayout();
             this.export_report.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.advancedDataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dgv_report_grpCtrl
@@ -74,10 +74,10 @@
             this.dgv_report_grpCtrl.Controls.Add(this.bpk_proyek);
             this.dgv_report_grpCtrl.Controls.Add(this.po_kantor);
             this.dgv_report_grpCtrl.Controls.Add(this.report);
-            this.dgv_report_grpCtrl.Location = new System.Drawing.Point(357, 76);
+            this.dgv_report_grpCtrl.Location = new System.Drawing.Point(357, 12);
             this.dgv_report_grpCtrl.Name = "dgv_report_grpCtrl";
             this.dgv_report_grpCtrl.SelectedIndex = 0;
-            this.dgv_report_grpCtrl.Size = new System.Drawing.Size(594, 254);
+            this.dgv_report_grpCtrl.Size = new System.Drawing.Size(594, 408);
             this.dgv_report_grpCtrl.TabIndex = 7;
             // 
             // breakdown_rap
@@ -88,10 +88,21 @@
             this.breakdown_rap.Location = new System.Drawing.Point(4, 22);
             this.breakdown_rap.Name = "breakdown_rap";
             this.breakdown_rap.Padding = new System.Windows.Forms.Padding(3);
-            this.breakdown_rap.Size = new System.Drawing.Size(586, 228);
+            this.breakdown_rap.Size = new System.Drawing.Size(586, 382);
             this.breakdown_rap.TabIndex = 0;
             this.breakdown_rap.Text = "BREAKDOWN RAP";
             this.breakdown_rap.UseVisualStyleBackColor = true;
+            // 
+            // advancedDataGridView1
+            // 
+            this.advancedDataGridView1.AutoGenerateContextFilters = true;
+            this.advancedDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.advancedDataGridView1.DateWithTime = false;
+            this.advancedDataGridView1.Location = new System.Drawing.Point(2, 2);
+            this.advancedDataGridView1.Name = "advancedDataGridView1";
+            this.advancedDataGridView1.Size = new System.Drawing.Size(580, 379);
+            this.advancedDataGridView1.TabIndex = 0;
+            this.advancedDataGridView1.TimeFilter = false;
             // 
             // breakdown_po
             // 
@@ -100,7 +111,7 @@
             this.breakdown_po.Location = new System.Drawing.Point(4, 22);
             this.breakdown_po.Name = "breakdown_po";
             this.breakdown_po.Padding = new System.Windows.Forms.Padding(3);
-            this.breakdown_po.Size = new System.Drawing.Size(586, 228);
+            this.breakdown_po.Size = new System.Drawing.Size(586, 382);
             this.breakdown_po.TabIndex = 1;
             this.breakdown_po.Text = "BREAKDOWN PO";
             this.breakdown_po.UseVisualStyleBackColor = true;
@@ -110,9 +121,9 @@
             this.dgv_breakdown_po_grpCtrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv_breakdown_po_grpCtrl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_breakdown_po_grpCtrl.Location = new System.Drawing.Point(3, 29);
+            this.dgv_breakdown_po_grpCtrl.Location = new System.Drawing.Point(0, 0);
             this.dgv_breakdown_po_grpCtrl.Name = "dgv_breakdown_po_grpCtrl";
-            this.dgv_breakdown_po_grpCtrl.Size = new System.Drawing.Size(580, 196);
+            this.dgv_breakdown_po_grpCtrl.Size = new System.Drawing.Size(580, 376);
             this.dgv_breakdown_po_grpCtrl.TabIndex = 0;
             // 
             // bpk_kantor
@@ -122,7 +133,7 @@
             this.bpk_kantor.Location = new System.Drawing.Point(4, 22);
             this.bpk_kantor.Name = "bpk_kantor";
             this.bpk_kantor.Padding = new System.Windows.Forms.Padding(3);
-            this.bpk_kantor.Size = new System.Drawing.Size(544, 239);
+            this.bpk_kantor.Size = new System.Drawing.Size(586, 382);
             this.bpk_kantor.TabIndex = 2;
             this.bpk_kantor.Text = "BPK KANTOR";
             this.bpk_kantor.UseVisualStyleBackColor = true;
@@ -132,9 +143,9 @@
             this.dgv_bpk_kantor_grpCtrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv_bpk_kantor_grpCtrl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_bpk_kantor_grpCtrl.Location = new System.Drawing.Point(3, 40);
+            this.dgv_bpk_kantor_grpCtrl.Location = new System.Drawing.Point(6, 180);
             this.dgv_bpk_kantor_grpCtrl.Name = "dgv_bpk_kantor_grpCtrl";
-            this.dgv_bpk_kantor_grpCtrl.Size = new System.Drawing.Size(538, 196);
+            this.dgv_bpk_kantor_grpCtrl.Size = new System.Drawing.Size(574, 196);
             this.dgv_bpk_kantor_grpCtrl.TabIndex = 0;
             // 
             // bpk_proyek
@@ -144,7 +155,7 @@
             this.bpk_proyek.Location = new System.Drawing.Point(4, 22);
             this.bpk_proyek.Name = "bpk_proyek";
             this.bpk_proyek.Padding = new System.Windows.Forms.Padding(3);
-            this.bpk_proyek.Size = new System.Drawing.Size(544, 239);
+            this.bpk_proyek.Size = new System.Drawing.Size(586, 382);
             this.bpk_proyek.TabIndex = 3;
             this.bpk_proyek.Text = "BPK PROYEK";
             this.bpk_proyek.UseVisualStyleBackColor = true;
@@ -154,9 +165,9 @@
             this.dgv_bpk_proyek_grpCtrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv_bpk_proyek_grpCtrl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_bpk_proyek_grpCtrl.Location = new System.Drawing.Point(3, 40);
+            this.dgv_bpk_proyek_grpCtrl.Location = new System.Drawing.Point(6, 180);
             this.dgv_bpk_proyek_grpCtrl.Name = "dgv_bpk_proyek_grpCtrl";
-            this.dgv_bpk_proyek_grpCtrl.Size = new System.Drawing.Size(538, 196);
+            this.dgv_bpk_proyek_grpCtrl.Size = new System.Drawing.Size(574, 196);
             this.dgv_bpk_proyek_grpCtrl.TabIndex = 0;
             // 
             // po_kantor
@@ -164,7 +175,7 @@
             this.po_kantor.Controls.Add(this.dgv_po_kantor_grpCtrl);
             this.po_kantor.Location = new System.Drawing.Point(4, 22);
             this.po_kantor.Name = "po_kantor";
-            this.po_kantor.Size = new System.Drawing.Size(544, 239);
+            this.po_kantor.Size = new System.Drawing.Size(586, 382);
             this.po_kantor.TabIndex = 4;
             this.po_kantor.Text = "PO KANTOR";
             this.po_kantor.UseVisualStyleBackColor = true;
@@ -174,16 +185,16 @@
             this.dgv_po_kantor_grpCtrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv_po_kantor_grpCtrl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_po_kantor_grpCtrl.Location = new System.Drawing.Point(3, 40);
+            this.dgv_po_kantor_grpCtrl.Location = new System.Drawing.Point(3, 183);
             this.dgv_po_kantor_grpCtrl.Name = "dgv_po_kantor_grpCtrl";
-            this.dgv_po_kantor_grpCtrl.Size = new System.Drawing.Size(538, 196);
+            this.dgv_po_kantor_grpCtrl.Size = new System.Drawing.Size(580, 196);
             this.dgv_po_kantor_grpCtrl.TabIndex = 0;
             // 
             // report
             // 
             this.report.Location = new System.Drawing.Point(4, 22);
             this.report.Name = "report";
-            this.report.Size = new System.Drawing.Size(544, 239);
+            this.report.Size = new System.Drawing.Size(586, 382);
             this.report.TabIndex = 5;
             this.report.Text = "REPORT";
             this.report.UseVisualStyleBackColor = true;
@@ -241,13 +252,14 @@
             // input_data
             // 
             this.input_data.Controls.Add(this.btn_BPK_proyek);
+            this.input_data.Controls.Add(this.export_report);
             this.input_data.Controls.Add(this.btn_breakdown_RAP);
             this.input_data.Controls.Add(this.btn_PO_kantor);
             this.input_data.Controls.Add(this.brn_breakdown_PO);
             this.input_data.Controls.Add(this.btn_BPK_kantor);
             this.input_data.Location = new System.Drawing.Point(3, 12);
             this.input_data.Name = "input_data";
-            this.input_data.Size = new System.Drawing.Size(348, 173);
+            this.input_data.Size = new System.Drawing.Size(348, 199);
             this.input_data.TabIndex = 9;
             this.input_data.TabStop = false;
             this.input_data.Text = "Input Data";
@@ -255,7 +267,7 @@
             // export_report
             // 
             this.export_report.Controls.Add(this.btn_report);
-            this.export_report.Location = new System.Drawing.Point(193, 200);
+            this.export_report.Location = new System.Drawing.Point(158, 124);
             this.export_report.Name = "export_report";
             this.export_report.Size = new System.Drawing.Size(158, 66);
             this.export_report.TabIndex = 5;
@@ -272,38 +284,27 @@
             this.btn_report.Text = "REPORT";
             this.btn_report.UseVisualStyleBackColor = true;
             // 
-            // grp_box_search
+            // groupBox1
             // 
-            this.grp_box_search.Location = new System.Drawing.Point(361, 12);
-            this.grp_box_search.Name = "grp_box_search";
-            this.grp_box_search.Size = new System.Drawing.Size(586, 63);
-            this.grp_box_search.TabIndex = 11;
-            this.grp_box_search.TabStop = false;
-            this.grp_box_search.Text = "Filter";
-            // 
-            // advancedDataGridView1
-            // 
-            this.advancedDataGridView1.AutoGenerateContextFilters = true;
-            this.advancedDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.advancedDataGridView1.DateWithTime = false;
-            this.advancedDataGridView1.Location = new System.Drawing.Point(2, 38);
-            this.advancedDataGridView1.Name = "advancedDataGridView1";
-            this.advancedDataGridView1.Size = new System.Drawing.Size(580, 196);
-            this.advancedDataGridView1.TabIndex = 0;
-            this.advancedDataGridView1.TimeFilter = false;
+            this.groupBox1.Location = new System.Drawing.Point(3, 226);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(348, 190);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // cost_control_uc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.grp_box_search);
-            this.Controls.Add(this.export_report);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.input_data);
             this.Controls.Add(this.dgv_report_grpCtrl);
             this.Name = "cost_control_uc";
             this.Size = new System.Drawing.Size(954, 423);
             this.dgv_report_grpCtrl.ResumeLayout(false);
             this.breakdown_rap.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.advancedDataGridView1)).EndInit();
             this.breakdown_po.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgv_breakdown_po_grpCtrl)).EndInit();
             this.bpk_kantor.ResumeLayout(false);
@@ -314,7 +315,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgv_po_kantor_grpCtrl)).EndInit();
             this.input_data.ResumeLayout(false);
             this.export_report.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.advancedDataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -339,7 +339,7 @@
         private System.Windows.Forms.GroupBox input_data;
         private System.Windows.Forms.GroupBox export_report;
         private System.Windows.Forms.Button btn_report;
-        private System.Windows.Forms.GroupBox grp_box_search;
         private ADGV.AdvancedDataGridView advancedDataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
